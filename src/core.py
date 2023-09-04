7c0ee5675a49ea2331e67f67c9023f62fb5a7278
TonyMasse <tony.masse@logrhythm.com>
2023-09-04 22:17:38 +0000

    Core - Better config loading

--- src/core.py
+++ src/core.py
@@ -8,12 +8,20 @@ logging.basicConfig(level=logging.INFO if os.environ.get('MODE') != 'DEV' else l
 logging.info('--------------')
 logging.info('ScriptableBeat - Starting')
 
-def read_config(config_file_path):
+# Get the base directory of the script
+base_script_dir = os.path.dirname(os.path.realpath(__file__))
+
+def read_config():
+    # Define the path to the config file
+    config_folder_path = '/beats/scriptablebeat/config' if os.environ.get('MODE') != 'DEV' else os.path.join(base_script_dir, '..', 'config.dev')
+    config_file_name = 'scriptablebeat.yml'
+    config_file_path = os.path.join(config_folder_path, config_file_name)
+
     logging.info('Reading configuration from "%s" ...', config_file_path)
     try:
         with open(config_file_path, 'r') as config_file:
             config_data = yaml.safe_load(config_file)
-            logging.info('Configuration loaded.', config_file_path)
+            logging.info('Configuration loaded.')
         return config_data
     except FileNotFoundError:
         logging.error('Config file "%s" not found.', config_file_path)
@@ -23,26 +31,18 @@ def read_config(config_file_path):
         return None
 
 if __name__ == "__main__":
-    # Define the path to the config file
-    config_file_path = '/beats/scriptablebeat/config' if os.environ.get('MODE') != 'DEV' else '../config.dev'
-    config_file_name = 'scriptablebeat.yml'
-    config_file_path = os.path.join(config_file_path, config_file_name)
-
     # Read the configuration
-    config = read_config(config_file_path)
+    config = read_config()
 
     if config:
         # Access specific configuration values
-        db_host = config.get('database', {}).get('host', 'localhost') # XXXX
-        db_port = config.get('database', {}).get('port', 3306) # XXXX
-        db_username = config.get('database', {}).get('username', 'myuser') # XXXX
-        db_password = config.get('database', {}).get('password', 'mypassword') # XXXX
-
+        script__first_run = config.get('scripts', {}).get('first_run', None)
+        script__run = config.get('scripts', {}).get('run', '')
+        script__scheduled = config.get('scripts', {}).get('scheduled', '')
         script_language = config.get('language', 'bash')
 
-        logging.debug('Database Configuration:')
-        logging.debug('Host: %s', db_host)
-        logging.debug('Port: %s', db_port)
-        logging.debug('Username: %s', db_username)
-        logging.debug('Password: %s', db_password)
+        logging.debug('Configuration:')
         logging.debug('script_language: %s', script_language)
+        logging.debug('script__first_run: %s', script__first_run)
+        logging.debug('script__run: %s', script__run)
+        logging.debug('script__scheduled: %s', script__scheduled)

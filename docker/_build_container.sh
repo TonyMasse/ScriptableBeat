3e3028a749b5bff5321989cda9f20dc481a60bd6
TonyMasse <tony.masse@logrhythm.com>
2023-09-04 18:03:58 +0100

    Docker - Update Build script

--- docker/_build_container.sh
+++ docker/_build_container.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Get the latest ubuntu image
+docker pull ubuntu:latest
+
+# Build and ship the container
 docker build -t tonymasse/scriptable_beat:v0.1 -t tonymasse/scriptable_beat:latest ./ $@
 docker push --all-tags tonymasse/scriptable_beat

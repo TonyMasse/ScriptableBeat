58a7c4441b10d043407182ae0ab665077557ba75
TonyMasse <tony.masse@logrhythm.com>
2023-09-05 20:29:38 +0000

    Sandpit - Testing `pyLogBeat`

---
+++ sandpit/lj-test-3.py
@@ -0,0 +1,20 @@
+from locallib.pyLogBeat import PyLogBeatClient
+
+message = {'@timestamp': '2018-01-02T01:02:03',  '@version': '1', 'message': 'hello world'}
+client = PyLogBeatClient('logstash', 5045, ssl_enable=False)
+client.connect()
+client.send([message])
+client.close()
+
+# Results in this in LogStash:
+# {
+#        "message" => "hello world",
+#          "event" => {
+#         "original" => "hello world"
+#     },
+#           "tags" => [
+#         [0] "beats_input_codec_plain_applied"
+#     ],
+#       "@version" => "1",
+#     "@timestamp" => 2018-01-02T01:02:03.000Z
+# }

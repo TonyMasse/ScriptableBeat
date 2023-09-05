ac1959be6d93eadc79e746afec661b31be6f9374
TonyMasse <tony.masse@logrhythm.com>
2023-09-05 22:01:10 +0000

    Sandpit - LogStash test start script

---
+++ sandpit/docker.run.logstash_test.sh
@@ -0,0 +1,4 @@
+# Testing logstash docker image
+# Helps debug Lumberjack messages being sent from ScriptableBeat
+
+docker run --rm -it --name logstash -p 5045:5044 docker.elastic.co/logstash/logstash:8.9.1

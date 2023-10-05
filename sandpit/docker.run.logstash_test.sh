2d4782f7e29df6f0cc985fc71392b32a125a9280
TonyMasse <tony.masse@logrhythm.com>
2023-10-05 13:00:56 +0000

    Sanpit - Weed out license messages from LogStash

--- sandpit/docker.run.logstash_test.sh
+++ sandpit/docker.run.logstash_test.sh
@@ -1,4 +1,5 @@
 # Testing logstash docker image
 # Helps debug Lumberjack messages being sent from ScriptableBeat
 
-docker run --rm -it --name logstash -p 5045:5044 docker.elastic.co/logstash/logstash:8.9.1
+# docker run --rm -it --name logstash -p 5045:5044 docker.elastic.co/logstash/logstash:8.9.1
+docker run --rm -it --name logstash -p 5045:5044 docker.elastic.co/logstash/logstash:8.9.1 | grep -v "logstash.licensechecker.licensereader"

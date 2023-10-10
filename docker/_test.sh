66a8e8677e83a7c28708950bed8b174a2140c820
TonyMasse <tony.masse@logrhythm.com>
2023-10-10 15:59:02 +0000

    Docker - Update `_test.sh` to use path as from host to run test on Dev config file

--- docker/_test.sh
+++ docker/_test.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 # Build (without publishing, not checking), run the container, Shell into it, then remove it
-./_build_container.sh --no-publish --no-check && docker run -d --name sb tonymasse/scriptable_beat:latest && docker exec -it sb bash && docker rm sb --force
+# ./_build_container.sh --no-publish --no-check && docker run -d --name sb tonymasse/scriptable_beat:latest && docker exec -it sb bash && docker rm sb --force
+
+# ./_build_container.sh --no-publish --no-check && \
+# docker run --detach --name sb --volume "/root/sb_dev/ScriptableBeat/config.dev/scriptablebeat.yml":"/beats/scriptablebeat/config/scriptablebeat.yml":ro tonymasse/scriptable_beat:latest && \
+# docker exec -it sb bash && docker rm sb --force
+
+./_build_container.sh --no-publish --no-check && \
+docker run --detach --name sb --volume "/var/lib/docker/volumes/sb_dev/_data/ScriptableBeat/config.dev/scriptablebeat.yml":"/beats/scriptablebeat/config/scriptablebeat.yml":ro tonymasse/scriptable_beat:latest && \
+docker exec -it sb bash && docker rm sb --force
